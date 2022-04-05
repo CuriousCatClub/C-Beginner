bda32f87f9bc1cca9484f97f4a58ed39e58f3724
curiouscatclub <curiouscatclub@protonmail.com>
2022-04-05 09:11:51 +0100

    Adding array to pointer arithmetic

--- 018-pointer-arithmetic.c
+++ 018-pointer-arithmetic.c
@@ -5,7 +5,14 @@ int main(void)
   int i;
   int *p;
   p = &i;
+  
+  int numbers[] = {10, 20, 30, 40, 50};
 
+  // Assign the first array element address to pointer
+  // The same can be done with: int *array_ptr = &numbers[0]
+  int *array_ptr = numbers;
+
+  
   printf("Size of int is = %zu bytes.\n", sizeof(int));
   printf("Address of p = %p\n", p);
 
@@ -14,5 +21,10 @@ int main(void)
 
   printf("\nNow address of p = %p\n", p);
 
+  printf("\n-------------------------------\n");
+  printf("\nReading int array elements with pointers.\n");
+  printf("1st array element: %d\n", *array_ptr);        // prints 10
+  printf("2nd array element: %d\n", *(array_ptr + 1));  // prints 20
+  
   return 0;
 }
